bfe804e5a03ee0cb2f2ba628cee0fc0b53a085f6
Joe Susnick <joesusnick@gmail.com>
2015-08-13 14:11:02 -0600

    initial commit

---
+++ MememeUdacity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MememeUdacity
+//
+//  Created by A658308 on 8/13/15.
+//  Copyright (c) 2015 Joe Susnick Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
