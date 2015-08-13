6f9b787d15a2d504f5968af9255336b7a3821158
Joe Susnick <joesusnick@gmail.com>
2015-08-13 16:40:51 -0600

    changed to use sliders

--- MememeUdacity/ViewController.swift
+++ MememeUdacity/ViewController.swift
@@ -11,9 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var colorView: UIView!
-    @IBOutlet weak var redControl: UISwitch!
-    @IBOutlet weak var greenControl: UISwitch!
-    @IBOutlet weak var blueControl: UISwitch!
+    @IBOutlet weak var redControl: UISlider!
+    @IBOutlet weak var greenControl: UISlider!
+    @IBOutlet weak var blueControl: UISlider!
     
     
     
@@ -25,27 +25,11 @@ class ViewController: UIViewController {
     
     @IBAction func changeColorComponent() {
         
-        let r: CGFloat = self.redControl.on ? 1 : 0
-        let g: CGFloat = self.greenControl.on ? 1 : 0
-        let b: CGFloat = self.blueControl.on ? 1 : 0
+        let r: CGFloat = CGFloat(redControl.value)
+        let g: CGFloat = CGFloat(greenControl.value)
+        let b: CGFloat = CGFloat(blueControl.value)
         
         colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
     }
-    
-    
-//    @IBAction func changeColorComponent() {
-//        
-//        // Make sure the program doesn't crash if the controls aren't connected
-//        if self.redControl == nil {
-//            return
-//        }
-//        
-//        let r: CGFloat = self.redControl.on ? 1 : 0
-//        let g: CGFloat = self.greenControl.on ? 1 : 0
-//        let b: CGFloat = self.blueControl.on ? 1 : 0
-//        
-//        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
-//    }
-
 }
 
