5954538f6d96eb8a2b0d4956f5455065c3eccb56
Joe Susnick <joesusnick@gmail.com>
2015-08-13 16:11:15 -0600

    color picker layout complete

--- MememeUdacity/ViewController.swift
+++ MememeUdacity/ViewController.swift
@@ -10,29 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var count = 0
-    @IBOutlet weak var label: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     
     }
     
-    @IBAction func incrementCount(sender: UIButton) {
-        sender.tag = 1
-        modifyCount(sender)
-    }
-    
-    @IBAction func decrementCounter(sender: UIButton) {
-        sender.tag = -1
-        modifyCount(sender)
-    }
-    
-    func modifyCount(sender: UIButton) {
-        println(sender)
-        count += sender.tag
-        label.text = "\(count)"
-    }
 
 }
 
